7bece9683cea699892c03cc42355722fc3899710
ricardoCuisine <202274231@edu.clg.qc.ca>
2025-03-13 14:01:48 -0400

    index

--- index.php
+++ index.php
@@ -3,13 +3,6 @@ require 'src/router.php';
 require 'src/functions.php';
 require 'src/configuration.php';
 require 'src/error-config.php';
-<<<<<<< HEAD
 require 'src/class/database.php';
-=======
-<<<<<<< HEAD
-require 'src/class/database.php';
-=======
->>>>>>> 7289bda1c276b9c249c4449d50d4cb7ce2390bfb
 
->>>>>>> d203a9df434b3bd75267a932cf210b99a836e514
 route(urlPath());
