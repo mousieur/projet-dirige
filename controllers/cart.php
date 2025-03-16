fbbd25333235bfa8d4405a30a2c983dba980a8d1
mousieur <gabmasse0504@gmail.com>
2025-03-16 14:07:54 -0400

    add htaccess file

--- controllers/cart.php
+++ controllers/cart.php
@@ -1,2 +1,15 @@
 <?php
-view('cart', []);
+sessionStart();
+require_once 'models/itemModel.php';
+require_once 'src/class/item.php';
+
+$db = Database::getInstance(CONFIGURATIONS['database'], DB_PARAMS);
+$pdo = $db->getPDO();
+
+$itemModel = new itemModel($pdo);
+$items = $itemModel->selectAll();
+
+view('cart', 
+[
+    'items' => $items
+]);
