a2122404eead637b8940a919de6e780fd67f5715
ricardoCuisine <202274231@edu.clg.qc.ca>
2025-03-20 13:06:09 -0400

    recherche

--- views/index.php
+++ views/index.php
@@ -7,7 +7,7 @@
             <div class="col-lg-2 bg-secondary text-dark border-end border-dark-subtle shadow p-4">
                 <form method="post">
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control" name="search" placeholder="Nom">
+                        <input type="text" class="form-control" name="Search" placeholder="Nom" value="<?=$search?>">
                         <button class="btn btn-outline-light" type="submit">
                             <i class="fa fa-search"></i>
                         </button>
@@ -16,27 +16,27 @@
                     <div class="fw-bold mb-2">Catégorie</div>
                     
                     <div class="form-check">
-                        <input type="checkbox" id="armes" name="armes" class="form-check-input">
+                        <input type="checkbox" id="armes" name="Arme" class="form-check-input" <?=$arme?>>
                         <label for="armes" class="form-check-label">Armes</label>
                     </div>
                     
                     <div class="form-check">
-                        <input type="checkbox" id="munitions" name="munitions" class="form-check-input">
+                        <input type="checkbox" id="munitions" name="Munition" class="form-check-input" <?=$munition?>>
                         <label for="munitions" class="form-check-label">Munitions</label>
                     </div>
                     
                     <div class="form-check">
-                        <input type="checkbox" id="armures" name="armures" class="form-check-input">
+                        <input type="checkbox" id="armures" name="Armure" class="form-check-input" <?=$armure?>>
                         <label for="armures" class="form-check-label">Armures</label>
                     </div>
                     
                     <div class="form-check">
-                        <input type="checkbox" id="nourritures" name="nourritures" class="form-check-input">
+                        <input type="checkbox" id="nourritures" name="Nourriture" class="form-check-input" <?=$nourriture?>>
                         <label for="nourritures" class="form-check-label">Nourritures</label>
                     </div>
                     
                     <div class="form-check">
-                        <input type="checkbox" id="medicaments" name="medicaments" class="form-check-input">
+                        <input type="checkbox" id="medicaments" name="Medicament" class="form-check-input" <?=$medicament?>>
                         <label for="medicaments" class="form-check-label">Médicaments</label>
                     </div>
                 </form>
@@ -50,47 +50,49 @@
             <!-- source de départ du template d'un item : https://templatemo.com/tm-559-zay-shop -->
             <div class="row d-flex">
                 <?php
+                if(!empty($items)) {
+                    foreach ($items as $item) {
+                        $totalStars = 0;
 
-                foreach ($items as $item) {
-                    $totalStars = 0;
-
-                    $comments = $commentModel->getCommentsByIdItem($item->idItem);
-                    foreach ($comments as $comment)
-                            $totalStars += $comment->etoiles;
-                    
-                    $avgStars = count($comments) > 0 ? floor($totalStars / count($comments)) : 0;
-                    
-                ?>
-                <div class="col-12 col-md-4 mb-4">
-                    <div class="card h-100">
-                        <a href="/details?idItem=<?= $item->idItem ?>">
-                            <img src="public/img/<?=$item->photo?>" class="card-img-top" alt="...">
-                        </a>
-                        <div class="card-body">
-                            <ul class="list-unstyled d-flex justify-content-between">
-                                <li>
-                                    <?php 
-                                    for ($i = 1; $i <= 5; $i++)
-                                        if($i <= $avgStars) 
-                                            echo '<i class="text-warning fa fa-star"></i>';
-                                        else
-                                            echo '<i class="text-muted fa fa-star"></i>';
-                                    ?>
-                                </li>
-                                <li class="text-muted text-right"><?= $item->prixUnitaire ?> $</li>
-                            </ul>
-                            <div class="d-flex justify-content-between">
-                                <a href="/details" class="h2 text-decoration-none text-dark"><?= $item->nomItem ?></a>
-                                <span class="text-light pt-2"><?= $item->poids ?> lbs</span>
+                        $comments = $commentModel->getCommentsByIdItem($item->idItem);
+                        foreach ($comments as $comment)
+                                $totalStars += $comment->etoiles;
+                        
+                        $avgStars = count($comments) > 0 ? floor($totalStars / count($comments)) : 0;
+                        
+                    ?>
+                    <div class="col-12 col-md-4 mb-4">
+                        <div class="card h-100">
+                            <a href="/details?idItem=<?= $item->idItem ?>">
+                                <img src="public/img/<?=$item->photo?>" class="img-responsive img-thumnails card-img-top" alt="...">
+                            </a>
+                            <div class="card-body">
+                                <ul class="list-unstyled d-flex justify-content-between">
+                                    <li>
+                                        <?php 
+                                        for ($i = 1; $i <= 5; $i++)
+                                            if($i <= $avgStars) 
+                                                echo '<i class="text-warning fa fa-star"></i>';
+                                            else
+                                                echo '<i class="text-muted fa fa-star"></i>';
+                                        ?>
+                                    </li>
+                                    <li class="text-muted text-right"><?= $item->prixUnitaire ?> $</li>
+                                </ul>
+                                <div class="d-flex justify-content-between">
+                                    <a href="/details" class="h2 text-decoration-none text-dark"><?= $item->nomItem ?></a>
+                                    <span class="text-light pt-2"><?= $item->poids ?> lbs</span>
+                                </div>
+                                <p class="card-text">
+                                <?= $item->quantiteStock ?> disponibles
+                                </p>
+                                <p class="text-muted">Avis (<?=count($comments)?>)</p>
                             </div>
-                            <p class="card-text">
-                            <?= $item->quantiteStock ?> disponibles
-                            </p>
-                            <p class="text-muted">Avis (<?=count($comments)?>)</p>
                         </div>
                     </div>
-                </div>
-                <?php }?>
+                    <?php
+                    }
+                }?>
             </div>
         </div>
     </div>
